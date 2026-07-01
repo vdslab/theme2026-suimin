d00f3e5b90f9207e1a9c1cc8d32942940c83aa73
ju.dz <hasejun218@gmail.com>
2026-07-01 16:23:22 +0900

    対応していない国を日本語に強制変換。

--- src/utils/countryName.js
+++ src/utils/countryName.js
@@ -5,8 +5,20 @@ import en from "i18n-iso-countries/langs/en.json";
 countries.registerLocale(ja);
 countries.registerLocale(en);
 
+const countryNameOverrides = {
+  "Tanzania, United Republic Of": "タンザニア",
+  "United States (Puerto Rico)": "プエルトリコ",
+  "United States (Hawaii)": "ハワイ",
+  Laos: "ラオス",
+  China: "中国",
+};
+
 export function toJapaneseCountryName(countryEn) {
   if (!countryEn) return "";
+
+  if (countryNameOverrides[countryEn]) {
+    return countryNameOverrides[countryEn];
+  }
 
   const code = countries.getAlpha2Code(countryEn, "en");
 
